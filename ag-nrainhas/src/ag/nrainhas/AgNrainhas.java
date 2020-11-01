9fe5a2ae376c0b46bc61689ae7169171b696e39c
Default37 <fernando_souza_vasconcelos@hotmail.com>
2020-11-01 11:25:29 -0300

    Projeto conjunto

--- ag-nrainhas/src/ag/nrainhas/AgNrainhas.java
+++ ag-nrainhas/src/ag/nrainhas/AgNrainhas.java
@@ -1,4 +1,5 @@
 package ag.nrainhas;
+import java.util.Scanner;
 
 /**
  * Algoritmo genético que resolve o problema das N-Rainhas
@@ -8,8 +9,19 @@ package ag.nrainhas;
 public class AgNrainhas {
 
     public static void main(String[] args) {
-        AlgoritmoGenetico ag = new AlgoritmoGenetico(8, 1000); //instancia
-        ag.executa(100, 4, 15, "roleta", 1); //executa
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Tamanho do grafo: ");
+        int ngrafo = scanner.nextInt();
+        System.out.print("Tamanho da população: ");
+        int npop = scanner.nextInt();
+        System.out.print("Número de gerações: ");
+        int ngen = scanner.nextInt();
+        System.out.print("Tamanho do crossover ");
+        int ncross = scanner.nextInt();
+        System.out.print("Taxa de mutação(0-100): ");
+        int taxa = scanner.nextInt();
+        AlgoritmoGenetico ag = new AlgoritmoGenetico(ngrafo, npop); //instancia
+        ag.executa(ngen, ncross, taxa, "roleta", 1); //executa
     }
     
 }
